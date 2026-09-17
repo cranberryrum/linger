69103b995ea653f97dc519d7927593d4fa94f589
Aditya Kolte <aditya.kolte@headout.com>
2026-09-17 12:12:16 +0530

    Initial Commit

---
+++ linger/lingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  lingerApp.swift
+//  linger
+//
+//  Created by kolte on 17/09/26.
+//
+
+import SwiftUI
+
+@main
+struct lingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
